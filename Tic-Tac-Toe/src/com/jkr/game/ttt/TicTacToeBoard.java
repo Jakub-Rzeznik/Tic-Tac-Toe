7cc73dcfcb51c3bb8196e69ff6fad4a276dffc48
Jakub_Rzeznik <Jakub_Rzeznik@EPPLKRAW0280.budapest.epam.com>
2016-04-27 22:11:47 +0200

    win condition

--- Tic-Tac-Toe/src/com/jkr/game/ttt/TicTacToeBoard.java
+++ Tic-Tac-Toe/src/com/jkr/game/ttt/TicTacToeBoard.java
@@ -13,7 +13,23 @@ class TicTacToeBoard {
 	}
 	
 	boolean isWinCondition(int i, int j) {
-		return true;
+		return (board[i][j]!=null) && (checkRow(i) || checkColumn(j) || (i-j==0 && checkForwardSlant()) || (i+j==2 && checkBackwardSlant()));
+	}
+	
+	private boolean checkRow(int i) {
+		return (board[i][0]==board[i][1]) && (board[i][1]==board[i][2]);
+	}
+	
+	private boolean checkColumn(int j) {
+		return (board[0][j]==board[1][j]) && (board[1][j]==board[2][j]);
+	}
+	
+	private boolean checkForwardSlant() {
+		return (board[0][0]==board[1][1]) && (board[1][1]==board[2][2]);
+	}
+	
+	private boolean checkBackwardSlant() {
+		return (board[0][2]==board[1][1]) && (board[1][1]==board[2][0]);
 	}
 	
 	private void assertCoordinates(int i, int j) throws IndexOutOfBoundsException {

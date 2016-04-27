0888485f19fc8d23489667d28b68ec55587b8523
Jakub_Rzeznik <Jakub_Rzeznik@EPPLKRAW0280.budapest.epam.com>
2016-04-27 22:07:54 +0200

    fixed TDD for win condition

--- Tic-Tac-Toe/tests/com/jkr/game/ttt/TicTacToeBoardTest.java
+++ Tic-Tac-Toe/tests/com/jkr/game/ttt/TicTacToeBoardTest.java
@@ -2,18 +2,29 @@ package com.jkr.game.ttt;
 
 import static com.jkr.game.ttt.Mark.O;
 import static com.jkr.game.ttt.Mark.X;
-import static org.junit.Assert.*;
+import static com.jkr.game.ttt.TicTacToeBoardTest.Pair.pair;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 
 public class TicTacToeBoardTest {
 	
-	private static Set<int[]> allowedPairs = Arrays.stream(new int[][]{{0,0}, {0,1}, {0,2}, {1,0}, {1,1}, {1,2}, {2,0}, {2,1}, {2,2}}).collect(Collectors.toSet());
+	private static Set<Pair> allowedPairs = new HashSet<>();
+	static {
+		allowedPairs.add(pair(0,0));
+		allowedPairs.add(pair(0,1));
+		allowedPairs.add(pair(0,2));
+		allowedPairs.add(pair(1,0));
+		allowedPairs.add(pair(1,1));
+		allowedPairs.add(pair(1,2));
+		allowedPairs.add(pair(2,0));
+		allowedPairs.add(pair(2,1));
+		allowedPairs.add(pair(2,2));
+	}
 
 	@Test
 	public void testValidatingAndPutting() {
@@ -46,7 +57,7 @@ public class TicTacToeBoardTest {
 	public void testNotSuccessful() {
 		TicTacToeBoard board = createFilledBoard(X, pair(1,0), pair(1,1));
 		assertWinForPairs(board);
-		board = createFilledBoard(X, pair(0,0));
+		board = createFilledBoard(X, Pair.pair(0,0));
 		assertWinForPairs(board);
 		board = createFilledBoard(O, pair(1,2), pair(0,2));
 		assertWinForPairs(board);
@@ -60,32 +71,54 @@ public class TicTacToeBoardTest {
 		assertWinForPairs(board);
 	}
 	
-	private static int[] pair(int i, int j) {
-		return new int[] {i,j};
-	}
-	private static TicTacToeBoard createFilledBoard(Mark mark, int[]... pairs) {
+	private static TicTacToeBoard createFilledBoard(Mark mark, Pair... pairs) {
 		TicTacToeBoard board = new TicTacToeBoard();
-		for (int[] pair : pairs) {
-			System.out.println("Adding to ("+pair[0]+","+pair[1]+")");
-			board.putMark(pair[0], pair[1], mark);
+		for (Pair pair : pairs) {
+			System.out.println("Adding to ("+pair.i+","+pair.j+")");
+			board.putMark(pair.i, pair.j, mark);
 		}
 		return board;
 	}
-	private static void assertWinForPairs(TicTacToeBoard board, int[]... pairs){
-		Set<int[]> clonedAllowedPairs = new HashSet<>(allowedPairs);
-		for (int[] pair : pairs) {
-			System.out.println("Checking TRUE ("+pair[0]+","+pair[1]+")");
-			assertTrue(board.isWinCondition(pair[0], pair[1]));
+	private static void assertWinForPairs(TicTacToeBoard board, Pair... pairs){
+		Set<Pair> clonedAllowedPairs = new HashSet<>(allowedPairs);
+		for (Pair pair : pairs) {
+			System.out.println("Checking TRUE ("+pair.i+","+pair.j+")");
+			assertTrue(board.isWinCondition(pair.i, pair.j));
 			clonedAllowedPairs.remove(pair);
 		}
-		for (int[] pair : clonedAllowedPairs) {
-			System.out.println("Checking FALSE ("+pair[0]+","+pair[1]+")");
-			assertFalse(board.isWinCondition(pair[0], pair[1]));
+		for (Pair pair : clonedAllowedPairs) {
+			System.out.println("Checking FALSE ("+pair.i+","+pair.j+")");
+			assertFalse(board.isWinCondition(pair.i, pair.j));
 		}
 		
 	}
-	private void testSuccessfulWin(Mark mark, int[]... pairs) {
+	private void testSuccessfulWin(Mark mark, Pair... pairs) {
 		TicTacToeBoard board = createFilledBoard(mark, pairs);
 		assertWinForPairs(board, pairs);
+	}
+	static final class Pair {
+		final int i,j;
+		private Pair(int arg0, int arg1) {
+			i=arg0;
+			j=arg1;
+		}
+		static Pair pair(int arg0, int arg1) {
+			return new Pair(arg0, arg1);
+		}
+		@Override
+		public int hashCode() {
+			return i;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj instanceof Pair) {
+				Pair other = (Pair) obj;
+				return (i==other.i) && (j==other.j);
+			}
+			return false;
+		}
 	}
 }

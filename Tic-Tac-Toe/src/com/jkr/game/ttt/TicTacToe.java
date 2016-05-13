36fb188cd233cf042477ca9a10f5181fd11d476b
Jakub_Rzeznik <Jakub_Rzeznik@EPPLKRAW0280.budapest.epam.com>
2016-05-13 11:29:24 +0200

    respond to MulitplayerGame interface change

--- Tic-Tac-Toe/src/com/jkr/game/ttt/TicTacToe.java
+++ Tic-Tac-Toe/src/com/jkr/game/ttt/TicTacToe.java
@@ -3,13 +3,9 @@ package com.jkr.game.ttt;
 import com.jkr.game.area.Board;
 import com.jkr.game.execution.TurnBasedBoardGame;
 import com.jkr.game.presentation.Presentation;
+import com.jkr.game.setup.MultiplayerGame;
 
-public class TicTacToe extends TurnBasedBoardGame<Mark, Character> {
-
-	@Override
-	public Comparable<?>[] getPlayerDiscriminators() {
-		return Mark.values();
-	}
+public class TicTacToe extends TurnBasedBoardGame<Mark, Character> implements MultiplayerGame<Mark, Character> {
 
 	@Override
 	public Board<Mark> prepareCleanBoard() {
@@ -19,6 +15,11 @@ public class TicTacToe extends TurnBasedBoardGame<Mark, Character> {
 	@Override
 	protected Presentation<int[], Character> getPresentation() {
 		return new TicTacToeBoardConsoleRepresentation(prepareCleanBoard());
+	}
+
+	@Override
+	public Mark[] getPlayerDiscriminators() {
+		return Mark.values();
 	}
 
 }
